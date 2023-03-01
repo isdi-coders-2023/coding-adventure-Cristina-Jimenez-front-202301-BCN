0188e74a781c3cd0718e0408517b939577ed531c
Cristina <cristinajmartin98@gmail.com>
2023-03-01 18:07:31 +0100

    Add Form logic

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,14 +1,47 @@
+import { useState } from "react";
 import Button from "../Button/Button";
 import FormStyled from "./FormStyled";
 
 const Form = (): JSX.Element => {
   const signUpText = "Sign Up";
+
+  const initialRegisterData = {
+    email: "",
+    password: "",
+  };
+
+  let registerFormData = new FormData();
+
+  const [registerData, setRegisterData] = useState(initialRegisterData);
+
+  const areFieldsEmpty =
+    registerData.email === "" || registerData.password === "";
+
+  const handleRegisterData = ({
+    target: { id, value },
+  }: React.ChangeEvent<HTMLInputElement>) => {
+    setRegisterData({
+      ...registerData,
+      [id]: value,
+    });
+  };
+
+  const handleImage = ({
+    target: { files },
+  }: React.ChangeEvent<HTMLInputElement>) => {
+    registerFormData.append("avatar", files![0]);
+  };
+
+  const onSubmitHandler = (event: React.FormEvent<HTMLFormElement>) => {};
+
   return (
-    <FormStyled className="register-form">
+    <FormStyled className="register-form" onSubmit={onSubmitHandler}>
       <div className="register-form__field">
         <label htmlFor="email">
           Email
           <input
+            value={registerData.email}
+            onChange={handleRegisterData}
             required
             autoComplete="off"
             type="text"
@@ -22,23 +55,26 @@ const Form = (): JSX.Element => {
         <label htmlFor="password">
           Password
           <input
+            onChange={handleRegisterData}
+            value={registerData.password}
             required
             type="password"
             id="password"
             placeholder="Introduce your password"
           />
         </label>
-        <span className="form-required-modal">Password is required</span>
+        <span className="form-required-modal form-required-modal--visible">
+          Password is required
+        </span>
       </div>
       <div className="register-form__field">
         <label htmlFor="image">
           Image
-          <input required type="file" id="image" />
+          <input required type="file" id="image" onChange={handleImage} />
         </label>
-        <span className="form-required-modal">Image is required</span>
       </div>
       <div className="register-form__submit">
-        <Button text={signUpText} />
+        <Button areFieldsEmpty={areFieldsEmpty} text={signUpText} />
         <span className="form-required-modal">Sorry, something went wrong</span>
       </div>
     </FormStyled>
