2f0287a382236be9b181cd45b4e1e48063f476bc
Cristina <cristinajmartin98@gmail.com>
2023-03-02 13:37:58 +0100

    Add test for email, password, and image inputs

--- src/components/Form/Form.test.tsx
+++ src/components/Form/Form.test.tsx
@@ -1,4 +1,5 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { ThemeProvider } from "styled-components";
 import { mainTheme } from "../../styles/mainTheme/mainTheme";
 import Form from "./Form";
@@ -59,6 +60,62 @@ describe("Given the Form component", () => {
       const button = screen.getByRole("button", { name: buttonText });
 
       expect(button).toBeInTheDocument();
+    });
+  });
+
+  describe("When the user writes 'hola@hola.com' on Email input", () => {
+    test("Then it should set input's value to 'hola@hola.com'", async () => {
+      const inputLabel = "Email";
+      const expectedUserEmail = "hola@hola.com";
+
+      render(
+        <ThemeProvider theme={mainTheme}>
+          <Form />
+        </ThemeProvider>
+      );
+
+      const emailInput = screen.getByLabelText(inputLabel);
+
+      await userEvent.type(emailInput, expectedUserEmail);
+
+      expect(emailInput).toHaveValue(expectedUserEmail);
+    });
+  });
+
+  describe("When the user writes 'hola1234' on Password input", () => {
+    test("Then it should set input's value to 'hola1234'", async () => {
+      const inputLabel = "Password";
+      const expectedUserPassword = "hola1234";
+
+      render(
+        <ThemeProvider theme={mainTheme}>
+          <Form />
+        </ThemeProvider>
+      );
+
+      const passwordInput = screen.getByLabelText(inputLabel);
+
+      await userEvent.type(passwordInput, expectedUserPassword);
+
+      expect(passwordInput).toHaveValue(expectedUserPassword);
+    });
+  });
+
+  describe("When the user ataches a file on image input", () => {
+    test("Then it should append the file to registerFormData", async () => {
+      const inputLabel = "Image";
+      const testFile = new File(["hello"], "hello.png", { type: "image/png" });
+
+      render(
+        <ThemeProvider theme={mainTheme}>
+          <Form />
+        </ThemeProvider>
+      );
+      const imageInput: HTMLInputElement = screen.getByLabelText(inputLabel);
+
+      userEvent.upload(imageInput, testFile);
+
+      expect(imageInput.files![0]).toStrictEqual(testFile);
     });
   });
 });
